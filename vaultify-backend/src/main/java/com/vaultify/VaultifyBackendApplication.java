4b53ae558f0a2429ea55726b731dd6a6e39e1737
Niranjan <niranjancb2035@gmail.com>
2026-05-04 16:33:16 +0530

    fixed main class

--- vaultify-backend/src/main/java/com/vaultify/VaultifyBackendApplication.java
+++ vaultify-backend/src/main/java/com/vaultify/VaultifyBackendApplication.java
@@ -1,15 +1,30 @@
 package com.vaultify;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+
+import jakarta.annotation.PostConstruct;
 
 @SpringBootApplication
 @EntityScan("com.vaultify.model")
 public class VaultifyBackendApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(VaultifyBackendApplication.class, args);
-	}
+	 @Value("${DB_URL:NOT_FOUND}")
+        private String dbUrl;
+
+        @PostConstruct
+        public void printDbUrl
+            
+        () {
+    System.out.println("DB_URL = " + dbUrl);
+        }
+
+    public static void main(String[] args) {
+
+       
+        SpringApplication.run(VaultifyBackendApplication.class, args);
+    }
 
 }
